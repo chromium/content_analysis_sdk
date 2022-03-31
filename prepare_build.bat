02dbda0654dda88194e5bd9c7c16ffb04dca409f
Eric Waldman <waldman@google.com>
2022-03-31 14:08:38 -0400

    Add acknowledgment and handshake messages, rework READMEs, change how protos are generated, adds new targets (#7)
    * Move proto generation into cmake and remove from prepare_build
    
    * Create acknowledgement and handshake proto messages
    
    * Move proto generation into cmake and remove from prepare_build
    
    * Fix cmake protoc call in windows
    
    * Fix dynamic protoc in cmake on linux
    
    * Move proto generation into cmake and remove from prepare_build
    
    * Fix cmake protoc call in windows
    
    * Fix dynamic protoc in cmake on linux
    
    * Add comments to the cmake protoc routine
    
    * Add comments to the cmake protoc routine
    
    * add comment about cmake protoc path
    
    * add comment about cmake protoc path
    
    * Create handshake and acknowledgment protos (#6)
    
    * Create acknowledgement and handshake proto messages
    
    * Move proto generation into cmake and remove from prepare_build
    
    * add protos build target to CMakeLists
    
    * update READMEs and add some comments
    
    * Fix typo
    
    * Remove stale comments from acknowledgement.proto
    
    * Remove stale comments from handshake.proto
    
    * Add comments to the cmake protoc routine
    
    * move handshake and acknowledgement into analysis.proto
    
    * change handshake and acknowledgement into enums
    
    * restore handshake and acknowledgement into messages
    
    * add periods to comments, in line with chrome source guidlines
    
    * Change status from required to optional in protobuf messages
    
    https://github.com/chromium/content_analysis_sdk/pull/9#discussion_r838706355
    > Consider sticking to "optional," see [go/required-fields-harmful](https://goto.google.com/required-fields-harmful) for explanation
    
    * Remove unknown status from handshake (already has optional). Set Failure/Success to 0/1.
    
    * move Cmakelists, prepare_build to project root, use <project_root>/build as default build dir, add possibility to run prepare_build on existing dir, moved build instruction to main README.md (#11)
    
    * move Cmakelists, prepare_build to project root
    
    fix paths in CMakeLists to be relative to project root
    
    Change prepare_build paths to be relative to project root. Use build as default build dir
    
    update comments with default build arg
    
    in prepare_build, remove requirement for build directory to not exist (allow running script on existing build dir)
    
    move build instructions to main README
    
    * fix default argument in prepare_build bash script
    
    * Add missing period in comment, as per Chromium Guideline.
    
    * Add comment on prepare_build.bat about default build dir
    
    * nit: spelling
    
    * Change status from required to optional in protobuf messages
    
    https://github.com/chromium/content_analysis_sdk/pull/9#discussion_r838706355
    > Consider sticking to "optional," see [go/required-fields-harmful](https://goto.google.com/required-fields-harmful) for explanation

--- prepare_build.bat
+++ prepare_build.bat
@@ -6,17 +6,24 @@ setlocal
 
 REM This script is meant to be run once to setup the example demo agent.
 REM Run it with one command line argument: the path to a directory where the
-REM demo agent will be built.  This should be a directory outside the SDK
-REM directory tree.  This directory must not already exist.
+REM demo agent will be built. This should be a directory outside the SDK
+REM directory tree. By default, if no directory is supplied, a directory
+REM named `build` in the project root will be used.
 REM
-REM Once the build is prepared, the demo binary is build using the command
+REM Once the build is prepared, the demo binary is built using the command
 REM `cmake --build <build-dir>`, where <build-dir> is the same argument given
 REM to this script.
 
-set BUILD_DIR=%~f1
-set DEMO_DIR=%~dp0
-call :ABSPATH "%DEMO_DIR%.." ROOT_DIR
+set ROOT_DIR=%~dp0
+call :ABSPATH "%ROOT_DIR%\demo" DEMO_DIR
 call :ABSPATH "%ROOT_DIR%\proto" PROTO_DIR
+
+REM BUILD_DIR defaults to $ROOT_DIR/build if no argument is provided.
+IF "%1" == "" (
+  call :ABSPATH "%ROOT_DIR%\build" BUILD_DIR
+) ELSE (
+  set BUILD_DIR=%~f1
+)
 
 echo .
 echo Root dir:   %ROOT_DIR%
@@ -25,31 +32,26 @@ echo Demo dir:   %DEMO_DIR%
 echo Proto dir:  %PROTO_DIR%
 echo .
 
-IF exist "%BUILD_DIR%" (
-  echo.
-  echo   ### Directory %1 must not exist.
-  echo.
-  EXIT /b
-)
-
+REM Prepare build directory
 mkdir "%BUILD_DIR%"
+REM Prepare protobuf out directory
+mkdir "%BUILD_DIR%\gen"
+REM Enter build directory
 cd /d "%BUILD_DIR%"
 
 REM Install vcpkg and use it to install Google Protocol Buffers.
-cmd/c git clone https://github.com/microsoft/vcpkg
-cmd/c .\vcpkg\bootstrap-vcpkg.bat -disableMetrics
+IF NOT exist .\vcpkg\ (
+  cmd/c git clone https://github.com/microsoft/vcpkg
+  cmd/c .\vcpkg\bootstrap-vcpkg.bat -disableMetrics
+) ELSE (
+  echo vcpkg is already installed.
+)
+REM Install any packages we want from vcpkg.
 cmd/c .\vcpkg\vcpkg install protobuf:x64-windows
-
-REM Generate proto files.
-mkdir gen
-.\vcpkg\installed\x64-windows\tools\protobuf\protoc.exe ^
-  --cpp_out=gen ^
-  --proto_path=%PROTO_DIR% ^
-  %PROTO_DIR%\content_analysis\sdk\analysis.proto
 
 REM Generate the build files.
 set CMAKE_TOOLCHAIN_FILE=./vcpkg/scripts/buildsystems/vcpkg.cmake
-cmake %DEMO_DIR%
+cmake %ROOT_DIR%
 
 echo.
 echo.
