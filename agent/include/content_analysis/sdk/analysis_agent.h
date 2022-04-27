c725bc22847442fa0776d726ee7b981b61222d1d
Roger Tawa <rogerta@chromium.org>
2022-04-27 15:49:57 -0400

    Tweak comment

--- agent/include/content_analysis/sdk/analysis_agent.h
+++ agent/include/content_analysis/sdk/analysis_agent.h
@@ -94,10 +94,9 @@ class Session {
 // one Agent can be created for a given partner URI.
 //
 // Agent instances should outlive all Session instances created with it.
-// Otherwise those sessions become invalid.  Agent instances are not thread
-// safe except for Stop() which can be called from any thread to shutdown the
-// agent.  Outstanding Sessions created from this agent may or may not still
-// complete.
+// Agent instances are not thread safe except for Stop() which can be called
+// from any thread to shutdown the agent.  Outstanding Sessions created from
+// this agent may or may not still complete.
 //
 // See the demo directory for an example of how to use this class.
 class Agent {
