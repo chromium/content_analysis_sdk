e388404b52d49a115d7e48763db1fe3bb9edd0cb
Nancy Xiao <nancylanxiao@chromium.org>
2022-04-28 14:52:34 -0700

    Add shell file.

--- agent/src/agent_test_shell.cc
+++ agent/src/agent_test_shell.cc
@@ -13,7 +13,7 @@ namespace testing {
         // Expect two strings not to be equal.
         EXPECT_STRNE("hello", "world");
         // Expect equality.
-        EXPECT_EQ(7 * 6, 43);
+        EXPECT_EQ(7 * 6, 42);
     }
 
 }  // namespace testing
