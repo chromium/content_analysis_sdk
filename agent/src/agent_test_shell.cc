ae358708ff649e39cbfc81fb26435a3e163537c3
Roger Tawa <rogerta@chromium.org>
2022-06-01 13:23:23 -0400

    Update to demos, cmake files and tests

--- agent/src/agent_test_shell.cc
+++ agent/src/agent_test_shell.cc
@@ -9,12 +9,12 @@ namespace sdk {
 
 namespace testing {
 
-    TEST(HelloTest, BasicAssertions) {
-        // Expect two strings not to be equal.
-        EXPECT_STRNE("hello", "world");
-        // Expect equality.
-        EXPECT_EQ(7 * 6, 42);
-    }
+TEST(HelloTest, BasicAssertions) {
+  // Expect two strings not to be equal.
+  EXPECT_STRNE("hello", "world");
+  // Expect equality.
+  EXPECT_EQ(7 * 6, 42);
+}
 
 }  // namespace testing
 
