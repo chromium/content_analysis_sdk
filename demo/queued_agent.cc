f2e1fd9d73172dc49eeb106edf4b924d19d20902
Roger Tawa <rogerta@chromium.org>
2022-06-08 15:57:34 -0400

    Replace platform specific code in demos with C++11 equivalents.

--- demo/queued_agent.cc
+++ demo/queued_agent.cc
@@ -2,14 +2,11 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#if !defined(WIN32)
-#include <pthread.h>
-#endif
-
 #include <fstream>
 #include <iostream>
 #include <queue>
 #include <string>
+#include <thread>
 
 #include "content_analysis/sdk/analysis_agent.h"
 #include "demo/handler.h"
@@ -34,11 +31,7 @@ class QueuingHandler : public Handler {
     request_queue_.push(std::move(event));
   }
 
-#if defined(WIN32)
-  static unsigned __stdcall ProcessRequests(void* qh) {
-#else
   static void* ProcessRequests(void* qh) {
-#endif
     QueuingHandler* handler = reinterpret_cast<QueuingHandler*>(qh);
 
     while (true) {
@@ -56,38 +49,15 @@ class QueuingHandler : public Handler {
   RequestQueue request_queue_;
 
   void StartBackgroundThread() {
-#if defined(WIN32)
-    // Start a background thread to process the queue.  This demo starts one
-    // thread but any number would work.
-    unsigned tid;
-    thread_ = reinterpret_cast<HANDLE>(_beginthreadex(
-        nullptr, 0, ProcessRequests, this, 0, &tid));
-#else
-    // Start a background thread to process the queue.  This demo starts one
-    // thread but any number would work.
-    pthread_attr_t attr;
-    pthread_attr_init(&attr);
-    pthread_create(&tid_, &attr, ProcessRequests, this);
-    pthread_attr_destroy(&attr);
-#endif
+    thread_ = std::make_unique<std::thread>(ProcessRequests, this);
   }
 
   void WaitForBackgroundThread() {
-#if defined(WIN32)
-    WaitForSingleObject(thread_, INFINITE);
-    CloseHandle(thread_);
-#else
-    void* res;
-    pthread_join(tid_, &res);
-#endif
+    thread_->join();
   }
 
   // Thread id of backgrond thread.
-#if defined(WIN32)
-  HANDLE thread_;
-#else
-  pthread_t tid_;
-#endif
+  std::unique_ptr<std::thread> thread_;
 };
 
 int main(int argc, char* argv[]) {
