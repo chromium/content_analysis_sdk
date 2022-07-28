4c4eac0e21eb8aaee95432ae0e61d38015fefada
Roger Tawa <roger@tawacentral.net>
2022-07-28 14:11:59 -0400

    Set the path for user-specific and not user-specific to match the tests paths in chrome (#56)
    Co-authored-by: Roger Tawa <rogerta@chromium.org>

--- demo/client.cc
+++ demo/client.cc
@@ -13,7 +13,14 @@ using content_analysis::sdk::ContentAnalysisRequest;
 using content_analysis::sdk::ContentAnalysisResponse;
 using content_analysis::sdk::ContentAnalysisAcknowledgement;
 
+// Different paths are used depending on whether this agent should run as a
+// use specific agent or not.  These values are chosen to match the test
+// values in chrome browser.
+constexpr char kPathUser[] = "path_user";
+constexpr char kPathSystem[] = "path_system";
+
 // Global app config.
+const char* path = kPathSystem;
 bool user_specific = false;
 
 // Paramters used to build the request.
@@ -62,6 +69,7 @@ bool ParseCommandLine(int argc, char* argv[]) {
     } else if (arg.find(kArgUrl) == 0) {
       url = arg.substr(strlen(kArgUrl));
     } else if (arg.find(kArgUserSpecific) == 0) {
+      path = kPathUser;
       user_specific = true;
     } else if (arg.find(kArgHelp) == 0) {
       return false;
@@ -218,7 +226,7 @@ int main(int argc, char* argv[]) {
   }
 
   // Each client uses a unique name to identify itself with Google Chrome.
-  auto client = Client::Create({"content_analysis_sdk", user_specific});
+  auto client = Client::Create({path, user_specific});
   if (!client) {
     std::cout << "[Demo] Error starting client" << std::endl;
     return 1;
